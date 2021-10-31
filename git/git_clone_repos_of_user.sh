c80809a9f471ae86baa6bfe349f76dd6a1f59c98
elsey <elsey@wonderland>
2021-10-31 13:59:26 +0800

    Test through git_clone_repos_of_user.sh

--- git/git_clone_repos_of_user.sh
+++ git/git_clone_repos_of_user.sh
@@ -2,5 +2,4 @@
 
 USER_NAME=$1
 
-curl https://api.github.com/users/$USER_NAME/repos | grep -e 'git_url*' | cut -d \" -f 4 | xargs -p 4 -L1 git clone
-
+curl https://api.github.com/users/$USER_NAME/repos | grep -e 'git_url*' | cut -d \" -f 4 | grep -e 'git_url*' | cut -d " -f 4 | sed 's/git:\/\//https:\/\//' | xargs -I '{}' -P 4 git clone '{}'

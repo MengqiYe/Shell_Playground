0e4f2780281c9384d2527d741beee8a0059bfeed
elsey <elsey@wonderland>
2021-11-04 14:40:13 +0800

    User git_url to exclude things.

--- git/git_clone_repos_of_org.sh
+++ git/git_clone_repos_of_org.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-USER_NAME=$1
-REPO_COUNT=$(curl -silent https://api.github.com/users/$USER_NAME | grep -e "public_repos" | cut -d \" -f 3 | cut -d \: -f 2 | cut -d \, -f 1)
+ORG_NAME=$1
+REPO_COUNT=$(curl -silent https://api.github.com/orgs/$ORG_NAME | grep -e "public_repos" | cut -d \" -f 3 | cut -d \: -f 2 | cut -d \, -f 1)
 PER_PAGE=32
 
 (( PAGE_COUNT=(REPO_COUNT + PER_PAGE - 1)/PER_PAGE ));
@@ -9,9 +9,11 @@ PER_PAGE=32
 for i in `seq 1 $PAGE_COUNT`;
 do
       # | grep -e 'html_url' \
-    curl -silent https://api.github.com/users/$USER_NAME/repos?page=$i \
+    curl -silent https://api.github.com/orgs/$ORG_NAME/repos?page=$i \
         | grep -e 'git_url*' | cut -d \" -f 4 | sed 's/git:\/\//https:\/\//' \
         | cut -d \" -f 4 \
         | xargs -I '{}' -P 4 git clone '{}' --depth=1
+
+
 done
 
